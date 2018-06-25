7b7dd178f978a9c36e019f718d131f71a494f3e1
[AL6981] <amylynnarrington@gmail.com>
2018-06-25 13:37:38 -0400

    ready for clinic, no dot map or tiles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const App = props => {
+
   return(
     <div className = "app">
-      <h1>Place Fluffballs here</h1>
+      <h1>Place Pet Components Here</h1>
     </div>
   )
+
 }
 
 export default App;
--- src/constants/data.js
+++ src/constants/data.js
@@ -1,57 +1,35 @@
-const animals = {
+const data = {
   dogs:
     [
       {
         id: 1,
         breed: "pit/lab",
         name: "Sir Geoffrey Drake Arrington",
-        image: "https://imgur.com/a/1vDT4Ze",
-        human: "AmyLynn"
-      },
-      {
-        id: 2,
-        breed: "ottoman with legs, maybe part hippo",
-        name: "Miss Islay Mae Arrington",
-        image: "https://imgur.com/a/IXNR3KI",
+        image: "https://i.imgur.com/Eyw9YNV.png",
         human: "AmyLynn"
       }
     ],
   cats:
     [
       {
-        id: 5,
+        id: 2,
         personality: "likes warming his rear on technology",
         name: "Bingo",
         image: "http://i.imgur.com/ntz2sQe.jpg",
         human: "AmyLynn"
-      },
-      {
-        id: 6,
-        personality: "likes mobile tech",
-        name: "Storrow",
-        image: "http://i.imgur.com/oSr7eBY.jpg",
-        human: "AmyLynn"
       }
     ],
-  roaches:
+  bugs:
     [
       {
-        id: 7,
-        species: "Madagascar Hissing Cockroach",
+        id: 3,
+        species: "Madagascar Hissing Cockroach, (Gromphadorhina portentosa)",
         personality: "territorial, loves the ladies",
         name: "Caelian",
-        image: "https://imgur.com/a/n3BToxX",
-        human: "AmyLynn"
-      },
-      {
-        id: 8,
-        species: "Madagascar Hissing Cockroach",
-        personality: "mellow, loves to reproduce",
-        name: "Aventine",
-        image: "https://imgur.com/a/LQiMOkK",
+        image: "https://i.imgur.com/QiEIzOw.png",
         human: "AmyLynn"
       }
     ]
   }
 
-export default animals;
+export default data;
