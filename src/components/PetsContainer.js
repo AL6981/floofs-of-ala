6a583f76623216e337bf580372acb0aa69752998
[AL6981] <amylynnarrington@gmail.com>
2018-06-27 14:32:15 -0400

    debugging data

--- src/components/PetsContainer.js
+++ src/components/PetsContainer.js
@@ -11,13 +11,24 @@ class PetsContainer extends React.Component {
   }
 
   resetPetSelection(pet) {
+    debugger
+    console.log(`pet passed in w/key of ${pet}`)
     this.setState({selectedPet: pet})
-    console.log(this.state.selectedPet)
   }
 
   render() {
+    console.log(this.state.selectedPet)
+    // console & debugger can read this.state inside render, outside an arrow function.
+    debugger
     let petArray = this.props.petData.map(pet => {
-
+      //we can console log state because arrow functions have access to "this", and we are still in ES6 inside our own code.
+    console.log(this.state.selectedPet)
+    debugger
+      // once inside the browser via a debugger, we lose access to "this", which means this.state is not accessible
+      //ES5 doesn't have arrow functions, it makes it a non-arrow function, which don't have access to "this"
+      //in ES5 we had to set a variable == "this" in order to access it inside the function
+      // this refers to whichever instance of the petcontainer we're referring to
+      // need babel because the browsers have been slow to be able to read ES6
       let className;
       if (pet.id === this.state.selectedPet) {
         className = "selected"
@@ -31,13 +42,13 @@ class PetsContainer extends React.Component {
 
       return(
         <PetTile
-          key = {pet.id}
-          name = {pet.name}
-          about = {pet.about}
-          image = {pet.image}
-          human = {pet.human}
-          className = {className}
-          handleClick = {handleClick}
+          key={pet.id}
+          name={pet.name}
+          about={pet.about}
+          image={pet.image}
+          human={pet.human}
+          className={className}
+          handleClick={handleClick}
         />
       )
     })
